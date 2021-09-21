051d255ded1bcc8fe9ee1b2154b191b32ba9df59
chosiyeon <chosiyeonn@gmail.com>
2021-09-21 22:58:57 +0900

    210921 Divide and Conquer 완료

---
+++ BAEKJOON/Divide and Conquer/*2448.py
@@ -0,0 +1,19 @@
+import math
+
+s = ['  *   ', ' * *  ', '***** ']
+
+
+def stars(shift):
+    c = len(s)
+    for i in range(c):
+        s.append(s[i] + s[i])
+        s[i] = ("   " * shift + s[i] + "   " * shift)
+
+n = int(input())
+k = int(math.log(int(n / 3), 2))
+
+for i in range(k):
+    stars(int(pow(2, i)))
+
+for i in range(n):
+    print(s[i])
