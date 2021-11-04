dde33731a8d5d891cc97b2d7cba2af1fdde3d0e4
chosiyeon <chosiyeonn@gmail.com>
2021-11-04 23:11:10 +0900

    211104 Re-DP(4)

--- BAEKJOON/RE/9465.py
+++ BAEKJOON/RE/9465.py
@@ -2,5 +2,22 @@ import sys
 input = sys.stdin.readline
 
 t = int(input())
-n = int(input())
 
+def sol(dp):
+    for i in range(1, len(dp[0])):
+        if i == 1:
+            dp[0][i] += dp[1][i-1]
+            dp[1][i] += dp[0][i-1]
+        else:
+            dp[0][i] += max(dp[1][i-1], dp[1][i-2])
+            dp[1][i] += max(dp[0][i-1], dp[0][i-2])
+    print(max(dp[0][-1], dp[1][-1]))
+
+
+for i in range(t):
+    n = int(input())
+    dp = []
+    for j in range(2):
+        arr = list(map(int, input().split()))
+        dp.append(arr)
+    sol(dp)
