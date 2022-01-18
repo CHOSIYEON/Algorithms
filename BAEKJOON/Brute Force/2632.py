0dede2d50f0c0cf80eb2b711d65702ff473d1d18
chosiyeon <tldusdk5@naver.com>
2022-01-18 15:59:40 +0900

    210118 Brute Force

--- BAEKJOON/Brute Force/2632.py
+++ BAEKJOON/Brute Force/2632.py
@@ -1,3 +1,59 @@
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+buy = int(input())
+m, n = map(int, input().split())
+pizza_a = [int(input()) for _ in range(m)]
+pizza_b = [int(input()) for _ in range(n)]
+
+sum_a = []
+sum_b = []
+
+sum_a.append(0)
+sum_b.append(0)
+sum_a.append(sum(pizza_a))
+sum_b.append(sum(pizza_b))
+
+for i in range(1, m):
+    start = 0
+    end = start + i
+    tmp = sum(pizza_a[start:end])
+
+    while start < m:
+        sum_a.append(tmp)
+        tmp -= pizza_a[start]
+        start += 1
+        tmp += pizza_a[end]
+        end += 1
+
+        if end >= m:
+            end %= m
+
+for i in range(1, n):
+    start = 0
+    end = start + i
+    tmp = sum(pizza_b[start:end])
+
+    while start < n:
+        sum_b.append(tmp)
+        tmp -= pizza_b[start]
+        start += 1
+        tmp += pizza_b[end]
+        end += 1
+
+        if end >= n:
+            end %= n
+
+c = Counter(sum_b)
+ans = 0
+
+for i in sum_a:
+    ans += c[buy-i]
+
+print(ans)
+
+######################### 시간 초
 # import sys
 # from collections import Counter
 # input = sys.stdin.readline
