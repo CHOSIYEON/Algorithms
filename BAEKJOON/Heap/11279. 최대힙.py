cf79cc3f0b8cab802627bc803b313c5685b0c4f2
Cho Si Yeon <tldusdk5@naver.com>
2022-03-21 12:01:36 +0900

    220321 힙

---
+++ BAEKJOON/Heap/11279. 최대힙.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+q = []
+
+for _ in range(n):
+    temp = int(input())
+    if temp == 0:
+        if len(q) > 0:
+            print(-heapq.heappop(q))
+        else:
+            print(0)
+    else:
+        heapq.heappush(q, -temp)
