9f2391bc2099a7d39f559f43f0a415b377664568
Cho Si Yeon <tldusdk5@naver.com>
2022-04-08 23:13:46 +0900

    DFS/BFS - 미로 탈출

---
+++ 이코테/RE/미로 탈출.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+n, m = map(int, input().split())
+maze = [list(map(int, input())) for _ in range(n)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if maze[nx][ny] == 1:
+                    maze[nx][ny] = maze[x][y] + 1
+                    queue.append((nx, ny))
+
+bfs(0, 0)
+print(maze[n-1][m-1])
