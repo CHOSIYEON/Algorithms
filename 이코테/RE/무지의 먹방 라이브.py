eec0fffb4a2daa845f49292513987213bd22d9c8
Cho Si Yeon <tldusdk5@naver.com>
2022-04-19 23:11:51 +0900

    그리디 - 무지의 먹방 라이브

---
+++ 이코테/RE/무지의 먹방 라이브.py
@@ -0,0 +1,23 @@
+import heapq
+
+def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
+
+    q = []
+
+    for idx, time in enumerate(food_times):
+        heapq.heappush(q, (time, idx + 1))
+
+    sum_value = 0
+    previous = 0
+    length = len(food_times)
+
+    while sum_value + ((q[0][0] - previous) * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1
+        previous = now
+
+    q.sort(key=lambda x: x[1])
+    return q[(k - sum_value) % length][1]
