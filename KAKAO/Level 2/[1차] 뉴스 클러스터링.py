4b7f63a4869eaa9122b9741092cb1a39d4abaf6f
Cho Si Yeon <tldusdk5@naver.com>
2022-05-04 23:54:11 +0900

    KAKAO Level 2 - [1차] 뉴스 클러스터링

--- KAKAO/Level 2/[1차] 뉴스 클러스터링.py
+++ KAKAO/Level 2/[1차] 뉴스 클러스터링.py
@@ -19,23 +19,13 @@ def solution(str1, str2):
             total += 1
             set2.append(temp.lower())
 
-    if len(set1) == 0 and len(set2) == 0:
-        return 65536
-
     set1, set2 = Counter(set1), Counter(set2)
     union_set = set1 + set2
-    inter = 0
 
-    for key in union_set.keys():
-        if union_set[key] > 1:
-            inter_cnt = min(set1[key], set2[key])
-            total -= inter_cnt
-            inter += inter_cnt
+    inter = list((set1 & set2).elements())
+    union = list((set1 | set2).elements())
 
-    if inter == 0:
-        return 0
-
-    answer = inter / total
-    answer = int(str(answer * 65536 * 10000)[:5])
-
-    return answer
+    if len(union) == 0 and len(inter) == 0:
+        return 65536
+    else:
+        return int(len(inter) / len(union) * 65536)
