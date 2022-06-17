cfc9893296eadb59eb18326805e40889f1a2f3f4
CHOSIYEON <tldusdk5@naver.com>
2022-06-17 21:54:15 +0900

    백준 그래프 - 2178. 미로 탐색

---
+++ BAEKJOON/Graph/2178. 미로 탐색(2).py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+queue = deque()
+queue.append((0, 0))
+
+while queue:
+    x, y = queue.popleft()
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < m:
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+print(graph[n-1][m-1])
